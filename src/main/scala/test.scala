162e1c3b09e144327d5c92a31a1d649b8b180bdd
wanzgheng <120445143@qq.com>
2020-08-23 08:37:56 +0800

    Initial commit

---
+++ src/main/scala/test.scala
@@ -0,0 +1,14 @@
+/**
+ * @description: TODO
+ * @author Rhett.wang
+ * @date 2020/8/23 8:35
+ */
+class test {
+
+
+}
+object test{
+  def main(args: Array[String]): Unit = {
+    println("hello java")
+  }
+}
